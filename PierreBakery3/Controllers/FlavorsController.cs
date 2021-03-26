8881fc10a731e2d5c3872e6b109cedfa0489b443
Lo-GR <diamondintheroth@gmail.com>
2021-03-26 13:35:43 -0700

    create flavor controller

---
+++ PierreBakery3/Controllers/FlavorsController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Bakery.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace Bakery.Controllers
+{
+  [Authorize]
+  public class FlavorsController : Controller
+  {
+    private readonly BakeryContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    public FlavorsController(UserManager<ApplicationUser> userManager, BakeryContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+    
+  }
+}
