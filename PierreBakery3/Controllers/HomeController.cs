98b40f2cc53b6ec941ccb77d6c7f064aa9f67bdb
Lo-GR <diamondintheroth@gmail.com>
2021-03-26 14:06:03 -0700

    amend homepage to add flavors index

--- PierreBakery3/Controllers/HomeController.cs
+++ PierreBakery3/Controllers/HomeController.cs
@@ -1,15 +1,31 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Bakery.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace Bakery.Controllers
 {
     public class HomeController : Controller
     {
+      private readonly BakeryContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    public HomeController(UserManager<ApplicationUser> userManager, BakeryContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
 
       [HttpGet("/")]
       public ActionResult Index()
       {
-        return View();
+        List<Flavor> flavors = _db.Flavors.ToList();
+        return View(flavors);
       }
-
     }
 }
