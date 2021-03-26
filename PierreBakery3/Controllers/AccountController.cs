28e2b132cec68dd7fbeac9dc2539f7a15cd02bc9
Lo-GR <diamondintheroth@gmail.com>
2021-03-26 11:30:26 -0700

    add logoff controllers

--- PierreBakery3/Controllers/AccountController.cs
+++ PierreBakery3/Controllers/AccountController.cs
@@ -57,5 +57,11 @@ namespace Bakery.Controllers
         return View();
       }
     }
+    [HttpPost]
+    public async Task<ActionResult> LogOff()
+    {
+      await _signInManager.SignOutAsync();
+      return RedirectToAction("Index");
+    }
   }
 }
