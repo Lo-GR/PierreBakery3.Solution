8ed56e9088f6cb92c274c7ef7a8681bea8202c78
Lo-GR <diamondintheroth@gmail.com>
2021-03-26 16:54:10 -0700

    add 'addflavor' and deleteflavor routes for treats

--- PierreBakery3/Controllers/TreatsController.cs
+++ PierreBakery3/Controllers/TreatsController.cs
@@ -44,5 +44,29 @@ namespace Bakery.Controllers
       .FirstOrDefault(treat => treat.TreatId == id);
       return View(model);
     }
+    public ActionResult AddFlavor(int id)
+    {
+      var model = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorName");
+      return View(model);
+    }
+    [HttpPost]
+    public ActionResult AddTreat(Treat treat, int FlavorId)
+    {
+      if (FlavorId != 0)
+      {
+      _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index", "Home");
+    }
+    [HttpPost]
+    public ActionResult DeleteFlavor(int joinId)
+    {
+      var joinEntry = _db.FlavorTreat.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
+      _db.FlavorTreat.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index", "Home");
+    }
   }
 }
