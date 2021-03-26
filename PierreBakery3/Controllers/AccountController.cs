73ff8b64da00e70899a8445f0e4d448acc4a14fb
Lo-GR <diamondintheroth@gmail.com>
2021-03-26 11:21:35 -0700

    add Register post controller

--- PierreBakery3/Controllers/AccountController.cs
+++ PierreBakery3/Controllers/AccountController.cs
@@ -25,6 +25,20 @@ namespace Bakery.Controllers
     {
       return View();
     }
+    [HttpPost]
+    public async Task<ActionResult> Register (RegisterViewModel model)
+    {
+      var user = new ApplicationUser {UserName = model.Email};
+      IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+      if (result.Succeeded)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+          return View();
+      }
+    }
     
   }
 }
